5ba2459a53a0377a6debe67595f2259cbce4f0d6
Moniiik15 <monikamilenkovic15@gmail.com>
2018-03-10 20:44:13 +0100

    Implementiran interfejs u klasi Menjacnica.
    Interfejs je nefunkcionalan.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,8 +1,11 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
-public class Menjacnica {
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs{
 	private LinkedList<Valuta> valute=new LinkedList<Valuta>();
 
 	
@@ -18,6 +21,24 @@ public class Menjacnica {
 	@Override
 	public String toString() {
 		return "Menjacnica > valute:" + valute;
+	}
+
+	@Override
+	public void dodajKurs(String valuta, GregorianCalendar datum, double prodajni, double srednji, double kupovni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(String valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKurs(String valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	
